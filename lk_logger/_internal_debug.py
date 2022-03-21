0c27cc0ad45aea0df494124cce50d4a1d7499df3
likianta <likianta@foxmail.com>
2022-03-21 18:09:13 +0800

    Simplify internal debug

--- lk_logger/_internal_debug.py
+++ lk_logger/_internal_debug.py
@@ -1,42 +1,13 @@
-"""
-usage:
-    from ._internal_debug import debug
-    
-    # 1
-    with debug.enable():
-        debug('some message')
-        
-    # 2
-    debug.enabled = True
-    debug('some message')
-    ...
-    debug.enabled = False
-    
-    # 3
-    debug('some message', condition=True)
-"""
-from contextlib import contextmanager
+from inspect import currentframe
 
+from .general import normpath
 from .general import std_print
 
 
-class Debugger:
-    
-    def __init__(self):
-        self.enabled = False
-    
-    @contextmanager
-    def enable(self, condition: bool = None):
-        if condition is None:
-            self.enabled = True
-        else:
-            self.enabled = condition
-        yield
-        self.enabled = False
-    
-    def __call__(self, *args, condition=None):
-        if self.enabled or condition:
-            std_print(*args)
-
-
-debug = Debugger()
+def debug(*args, condition=True):
+    frame = currentframe().f_back
+    filepath = normpath(frame.f_globals["__file__"])
+    lineno = frame.f_lineno
+    source = '{}:{}'.format(filepath, lineno)
+    if condition:
+        std_print(source, '[LKDEBUG]', *args)
