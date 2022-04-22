11a99aed5a85f1389a91d5264c34478675474393
likianta <likianta@foxmail.com>
2022-04-22 18:04:53 +0800

    Upgrade version number

--- lk_logger/__init__.py
+++ lk_logger/__init__.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
+from .general import std_print
 from .global_controls import disable
 from .global_controls import enable
 from .global_controls import setup
 from .global_controls import unload
 from .logger import lk
 
-__version__ = '5.0.2'
+__version__ = '5.0.3'
