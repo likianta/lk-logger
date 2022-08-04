7a34545731b2168f74b6383d6abd0ba5b9aaba57
likianta <likianta@foxmail.com>
2022-08-04 13:24:27 +0800

    Work with IPython use case

--- examples/do_not_show_source_and_funcname.py
+++ examples/do_not_show_source_and_funcname.py
@@ -19,3 +19,18 @@ print('hello world')
 
 a, b = 1, 2
 print(a, b, a + b)
+
+# -----------------------------------------------------------------------------
+
+"""
+this example is often used for an ipython console.
+here is a classic use case:
+    import lk_logger
+    from IPython import start_ipython  # pip install ipython
+    
+    lk_logger.setup(quiet=True,
+                    show_source=False,
+                    show_funcname=False,
+                    show_varnames=False)
+    start_ipython(user_ns=globals())
+"""
