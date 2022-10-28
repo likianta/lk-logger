7687751a6becbd06f632bf979c505d6ddb45cf8c
likianta <likianta@foxmail.com>
2022-10-28 17:22:31 +0800

    Test `:s` mark

---
+++ examples/short_mark_effect.py
@@ -0,0 +1,10 @@
+import lk_logger
+lk_logger.setup(quiet=True, show_varnames=True)
+
+a, b = 1, 2
+print(':vs', a, b)
+print(':v1s', a, b)
+print(':v2s', a, b)
+print(':v3s', a, b)
+print(':v4s', a, b)
+print(':v5s', a, b)
