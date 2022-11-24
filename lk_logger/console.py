27dc19ee1dcdc846829b98a798c27840d76da6a1
likianta <likianta@foxmail.com>
2022-11-24 15:22:32 +0800

    Add `**_` param to `console.print`

--- lk_logger/console.py
+++ lk_logger/console.py
@@ -1,4 +1,4 @@
-from functools import partial
+from typing import Any
 
 from rich.console import Console as BaseConsole
 
@@ -23,8 +23,18 @@ class Console(BaseConsole):
         #   if width longer than default, use single line style; otherwise
         #   split sourcemap and message into different lines.
         pass
+    
+    def print(self, *objects: Any, sep=" ", end="\n", style=None, justify=None,
+              overflow=None, no_wrap=None, emoji=None, markup=None,
+              highlight=None, width=None, height=None, crop=True,
+              soft_wrap=True, new_line_start=False, **_) -> None:
+        super().print(*objects, sep=sep, end=end, style=style, justify=justify,
+                      overflow=overflow, no_wrap=no_wrap, emoji=emoji,
+                      markup=markup, highlight=highlight, width=width,
+                      height=height, crop=crop, soft_wrap=soft_wrap,
+                      new_line_start=new_line_start)
 
 
 console = Console()
-con_print = partial(console.print, soft_wrap=True)
-con_error = partial(console.print_exception)
+con_print = console.print
+con_error = console.print_exception
