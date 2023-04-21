0acc898bae2264ee9c0f818c22e68a4a908222d4
likianta <likianta@foxmail.com>
2023-04-21 18:58:29 +0800

    Add alias to control

--- lk_logger/__init__.py
+++ lk_logger/__init__.py
@@ -1,10 +1,13 @@
 from . import console
 from ._print import bprint
 from .control import disable
+from .control import disable as mute
 from .control import enable
+from .control import enable as unmute
 from .control import setup
 from .control import start_ipython
 from .control import unload
+from .control import unload as restore_builtin_print
 from .control import update
 from .logger import lk
 from .pipeline import pipeline
