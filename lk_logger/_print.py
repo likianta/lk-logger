4abebb35b62540a8d330f20d720b1b3778d4c160
Likianta <likianta@foxmail.com>
2023-12-04 20:09:39 +0800

    Minor update

--- lk_logger/_print.py
+++ lk_logger/_print.py
@@ -1,6 +1,6 @@
 import builtins
+import os.path
 from inspect import currentframe
-from os import name as os_name
 
 bprint = builtins.print
 std_print = builtins.print
@@ -15,10 +15,5 @@ def debug(*args) -> None:
     bprint(source, '[LKDEBUG]', *args)
 
 
-_is_win = os_name == 'nt'
-
-
 def _normpath(path: str) -> str:
-    if _is_win:
-        return path.replace('\\', '/').rstrip('/')
-    return path.rstrip('/')
+    return os.path.relpath(path).replace('\\', '/').rstrip('/')
