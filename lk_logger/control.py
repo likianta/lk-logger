0a77ae4392d2da13aa47adb48296f90af23d15e5
likianta <likianta@foxmail.com>
2024-10-23 15:43:47 +0800

    Merge controls

--- lk_logger/control.py
+++ lk_logger/control.py
@@ -1,64 +1,52 @@
 import builtins
 import typing as t
+from contextlib import contextmanager
+from time import time
 
-from . import control2
 from .logger import logger
 from .printer import bprint
 
-status = 'unloaded'  # literal['enabled', 'disabled', 'unloaded']
+# -----------------------------------------------------------------------------
+# global controls
+
+_builtin_input = builtins.input
 _has_welcome_message_shown = False
-_binput = builtins.input
 
 
-def setup(
-    *,
-    quiet: bool = False,
-    clear_preset: bool = False,
-    _stdout: t.Callable = None,  # TODO: experimental
-    **kwargs
-) -> None:
+def setup(*, quiet: bool = False, clear_preset: bool = False, **kwargs) -> None:
     """
-    args:
+    params:
         quiet:
-            True: show a welcome message in caller side.
-            False: do not show.
-
-            note: the welcome message is shown only once, if caller calls this
-                function multi times, only the first time when passes
-                `quiet=True` will show this message.
-            tip: if you are developing an intermediate/supporting library, it
-                is recommended to set `quiet=True`.
-        clear_preset:
-        kwargs: see `./logger.py > LoggingConfig`.
+            - True: show a welcome message in caller side.
+            - False: do not show.
+            note: the welcome message is shown only once, if this function is -
+            called multiple times, only the first time of which passes -
+            `quiet=False` will show welcome message.
+        kwargs: see `.config.LoggingConfig._preset_conf`.
     """
-    global _has_welcome_message_shown, status
+    global _has_welcome_message_shown
     
     logger.configure(clear_preset, **kwargs)
-    setattr(builtins, 'print', _stdout or logger.log)
-    setattr(builtins, 'input', control2.input)
+    setattr(builtins, 'print', logger.log)
+    setattr(builtins, 'input', input)
     
     if not quiet and not _has_welcome_message_shown:
         _has_welcome_message_shown = True
         print('lk-logger is ready', ':v3sp')
-    
-    status = 'enabled'
 
 
-def update(clear_preset=False, **kwargs) -> None:
+def update(clear_preset: bool = False, **kwargs) -> None:
     logger.configure(clear_preset, **kwargs)
 
 
 def unload() -> None:
     setattr(builtins, 'print', bprint)
-    setattr(builtins, 'input', _binput)
-    global status
-    status = 'unloaded'
+    setattr(builtins, 'input', _builtin_input)
 
 
 def enable() -> None:
     setattr(builtins, 'print', logger.log)
-    global status
-    status = 'enabled'
+    setattr(builtins, 'input', _builtin_input)
 
 
 reload = enable
@@ -66,22 +54,75 @@ reload = enable
 
 def disable() -> None:
     setattr(builtins, 'print', lambda *_, **__: None)
-    global status
-    status = 'disabled'
+    setattr(builtins, 'input', _builtin_input)
+
+
+# -----------------------------------------------------------------------------
+# local controls
+
+# noinspection PyProtectedMember
+@contextmanager
+def counting() -> t.ContextManager:
+    logger._markup_analyzer._counter.reset_simple_count()
+    yield
+    logger._markup_analyzer._counter.reset_simple_count()
+
+
+# noinspection PyProtectedMember
+@contextmanager
+def delay() -> t.ContextManager:
+    logger._control['stash_outputs'] = True
+    yield
+    if logger._message_queue:
+        for (msg, is_raw, kwargs) in logger._message_queue:
+            logger._print(msg, is_raw, **kwargs)
+    logger._message_queue.clear()
+    logger._control['stash_outputs'] = False
+
+
+# noinspection PyProtectedMember
+@contextmanager
+def elevate_caller_stack() -> t.ContextManager:
+    logger._control['caller_layer_offset'] += 1
+    yield
+    logger._control['caller_layer_offset'] -= 1
+
+
+@contextmanager
+def mute() -> t.ContextManager:
+    _backup = builtins.print
+    builtins.print = lambda *_, **__: None
+    yield
+    builtins.print = _backup
+
+
+# noinspection PyProtectedMember
+@contextmanager
+def timing(sum_up: bool = False, exit_msg: str = None) -> t.ContextManager:
+    logger._markup_analyzer._simple_time = time()
+    yield
+    if exit_msg:
+        print(':p2ts', exit_msg)
+    elif sum_up:
+        print(':p2tsv', 'over')
 
 
 # -----------------------------------------------------------------------------
 # other
 
-def start_ipython(
-        user_ns: t.Dict[str, t.Any] = None
-) -> None:
-    if _is_ipython_mode():
+def input(prompt: str = '', flush_scheme: int = 0) -> str:
+    print(f':f{flush_scheme}s')
+    return _builtin_input(prompt)
+
+
+def start_ipython(user_ns: t.Dict[str, t.Any] = None) -> None:
+    if getattr(builtins, '__IPYTHON__', False):
+        # we are already in ipython environment.
         return
     try:
         import IPython  # noqa
     except (ImportError, ModuleNotFoundError) as e:
-        print('ipython is not installed!', ':pv4')
+        print('ipython is not installed!', ':pv8')
         raise e
     else:
         import sys
@@ -93,17 +134,26 @@ def start_ipython(
     
     pipeline.add(IPython, bprint, scope=True)
     
-    backups = {
+    backup = {
         'lklogger_config': logger.config.copy(),
         'sys.argv'       : sys.argv.copy(),
     }
     
-    setup(quiet=True, clear_preset=True,
-          show_source=False, show_funcname=False, show_varnames=False)
+    setup(
+        quiet=True,
+        clear_preset=True,
+        show_source=False,
+        show_funcname=False,
+        show_varnames=False,
+    )
     sys.argv = ['']  # avoid ipython to parse `sys.argv`.
     
     app = TerminalIPythonApp.instance(
-        user_ns={'print': logger.log, '__USERNS__': user_ns, **(user_ns or {})}
+        user_ns={
+            'print'     : logger.log,
+            '__USERNS__': user_ns,
+            **(user_ns or {})
+        }
     )
     app.initialize()
     
@@ -114,10 +164,6 @@ def start_ipython(
     app.start()
     
     # afterwards
-    logger.configure(**backups['lklogger_config'])
-    sys.argv = backups['sys.argv']
-    del backups
-
-
-def _is_ipython_mode() -> bool:
-    return getattr(builtins, '__IPYTHON__', False)
+    logger.configure(**backup['lklogger_config'])
+    sys.argv = backup['sys.argv']
+    del backup
