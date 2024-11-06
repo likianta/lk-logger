964d4c8a26f7b6cbcc851730747c00663240a9c1
likianta <likianta@foxmail.com>
2024-11-06 10:10:10 +0800

    Minor fix progress

--- lk_logger/__init__.py
+++ lk_logger/__init__.py
@@ -41,4 +41,4 @@ def _init() -> None:
 
 
 _init()
-__version__ = '6.0.1'
+__version__ = '6.0.2'
--- lk_logger/progress.py
+++ lk_logger/progress.py
@@ -7,7 +7,8 @@ import typing as t
 from contextlib import contextmanager
 
 import rich.progress
-from .logger import logger
+
+from .control import delay
 
 Item = t.TypeVar('Item', bound=t.Any)
 
@@ -16,7 +17,7 @@ def track(
     sequence: t.Union[t.Sequence[Item], t.Iterable[Item], t.Iterator[Item]],
     description: str = 'working...'
 ) -> t.Iterator[Item]:
-    with logger.delay():
+    with delay():
         yield from rich.progress.track(sequence, description)
 
 
@@ -26,7 +27,7 @@ def spinner(desc: str = 'working...') -> t.Iterator:
     # https://github.com/Textualize/rich/issues/1054
     cols = rich.progress.Progress.get_default_columns()
     cols[-1].elapsed_when_finished = True  # noqa
-    with logger.delay():
+    with delay():
         with rich.progress.Progress(*cols) as prog:
             task = prog.add_task(desc, total=None)
             yield
